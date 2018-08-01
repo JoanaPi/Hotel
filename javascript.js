7557c2beb6b5ac81c6fe6dbe3ebbbf52d47c3d1f
JoanaPi <joannapilakowska@gmail.com>
2018-08-01 17:11:08 +0200

    'all

---
+++ javascript.js
@@ -0,0 +1,22 @@
+function slider () {
+	
+	$current = $('.slider img.active');
+	if($current.lenght==0){
+	$('.slider img:first-child').addClass('active');
+	
+	} else 
+	{
+	$next = $current.removeClass('active').next();
+	if($next.length==0) {
+	$('.slider img:first-child').addClass('active');
+	}else {
+	$next.addClass('active');
+	
+	
+	
+	}
+	}
+	
+	
+	}
+	setInterval(slider,5000); 
